49f7e6403a7860a5081d7c338771cc13b6af4c15
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-21 08:08:08 -0700

    tests run and passed

--- Calendar.Tests/ModelTests/LeapYearTests.cs
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -18,5 +18,17 @@ namespace Calendar.Tests
       LeapYear testLeapYear = new LeapYear();
       Assert.AreEqual(false, testLeapYear.IsLeapYear(1999));
     }
+    [TestMethod]
+    public void IsLeapYear_MultiplesOfOneHundred_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
+    }
+    [TestMethod]
+    public void IsLeapYear_MultiplesOfFourHundred_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2000));
+    }
   }
 }
